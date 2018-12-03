ab31a67f30ccd3a0d93b71f7eb2de06daf83abd7
Tony <2296458423@qq.com>
2018-12-03 13:08:27 +0800

    修改首页，添加部分类别

--- config.py
+++ config.py
@@ -11,15 +11,19 @@ class Config(object):
     LOG_LEVEL = logging.DEBUG
     SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQU8yCM6xX9Yjq52v54g+HVoknA"
     # 数据库的配置信息
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/blogs?charset=utf8mb4"
+    # SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/blogs?charset=utf8mb4"
+    SQLALCHEMY_DATABASE_URI = "mysql://oychao:oychao1988@127.0.0.1:3306/Blogs?charset=utf8mb4"
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     REDIS_HOST = '127.0.0.1'
     REDIS_PORT = 6379
+    REDIS_PASSWORD = 'oychao1988'
+    # REDIS_PASSWORD = ''
+    # REDIS_URL = 'redis://:oychao@localhost:6379/'
     # flask_session的配置信息
     SESSION_TYPE = "redis"  # 指定 session 保存到 redis 中
     SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)  # 使用 redis 的实例
     PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
 
 
